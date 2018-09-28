58931306372face6ebe591d63862486e14079ee5
Angelo Luz <angelogl@gmail.com>
2018-09-27 21:33:35 -0300

    update Atividade.MD

---
+++ src/test/java/local/model/GuerreiroTest.java
@@ -0,0 +1,24 @@
+package local.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuerreiroTest {
+    @Test
+    void atacar() {
+    }
+
+    @Test
+    void contraAtacar() {
+    }
+
+    @Test
+    void aprimorarHabilidadePrincipal() {
+    }
+
+    @Test
+    void regenerarVida() {
+    }
+
+}
